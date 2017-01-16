8bcdcc99964eee9e1e35109b1cf92a7980903d5b
Jarkko Pajuniemi <jarkko.pajuniemi@outlook.com>
2017-01-16 19:12:31 +0200

    uusi

---
+++ helloworld/helloworld/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anna numero: ");
+            string line = Console.ReadLine();
+            int luku;
+
+            bool result = Int32.TryParse(line, out luku);
+            if (result)
+            {
+                switch (luku)
+                {
+                    case 1: Console.WriteLine("yksi"); break;
+                    case 2: Console.WriteLine("kaksi"); break;
+                    case 3: Console.WriteLine("kolme"); break;
+                    default: Console.WriteLine("joku muu luku"); break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Voisit antaa numeron");
+            }
+            Console.ReadLine();
+        }
+    }
+}
